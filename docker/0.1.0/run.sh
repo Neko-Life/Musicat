a6b01986903ff1229edf5bc894903147dd9b3722
Neko-Life <nekolife123579@gmail.com>
2022-10-06 15:07:45 +0700

    docker: Fix can't find user

--- docker/0.1.0/run.sh
+++ docker/0.1.0/run.sh
@@ -5,7 +5,7 @@ if [ check != 0 ]; then
     mkdir -p ../../exe/music
 fi
 
-sudo docker run --name musicat -it --rm -v $(pwd)/../../exe:/root/musicat/exe/conf -w /root/musicat/exe \
+docker run --name musicat -it --rm -v $(pwd)/../../exe:/root/musicat/exe/conf -w /root/musicat/exe \
     -u $(id -nu):$(id -ng) --memory-reservation 256mb -m 256mb --memory-swap 0 \
     shasha/musicat:0.1.0 /bin/bash -c 'ln -rs /root/musicat/exe/conf/sha_conf.json /root/musicat/exe/sha_conf.json && \
     ln -rs /root/musicat/exe/conf/music /root/musicat/exe/music && ./Shasha' $@
