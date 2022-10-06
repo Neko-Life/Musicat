df379124240427f816a7ae16d76827692e9d2404
Neko-Life <nekolife123579@gmail.com>
2022-10-06 13:46:14 +0700

    Fix can't access music dir

--- docker/0.1.0/run.sh
+++ docker/0.1.0/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-docker run --name musicat -it --rm -v $(pwd)/../../exe:/root/musicat/exe/conf -w /root/musicat/exe/conf \
+docker run --name musicat -it --rm -v $(pwd)/../../exe:/root/musicat/exe/conf -w /root/musicat/exe \
     -u $(id -nu):$(id -ng) --memory-reservation 256mb -m 256mb --memory-swap 0 \
-    shasha/musicat:0.1.0 ../Shasha $@
+    shasha/musicat:0.1.0 /bin/bash -c 'ln -rs /root/musicat/exe/conf/sha_conf.json /root/musicat/exe/sha_conf.json && \
+    ln -rs /root/musicat/exe/conf/music /root/musicat/exe/music && ./Shasha' $@
