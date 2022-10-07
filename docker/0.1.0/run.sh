bd909ae854a3e6302273bff762bd24a9914d6282
Neko-Life <nekolife123579@gmail.com>
2022-10-07 08:11:50 +0700

    docker: fix flags value syntax

--- docker/0.1.0/run.sh
+++ docker/0.1.0/run.sh
@@ -6,6 +6,6 @@ if [ check != 0 ]; then
 fi
 
 docker run --name musicat -it --rm -v $(pwd)/../../exe:/root/musicat/exe/conf -w /root/musicat/exe \
-    -u $(id -nu):$(id -ng) --memory-reservation 256mb -m 256mb --memory-swap 0 \
+    -u $(id -nu):$(id -ng) --memory-reservation 200M -m 256M --memory-swap 512M \
     shasha/musicat:0.1.0 /bin/bash -c 'ln -rs /root/musicat/exe/conf/sha_conf.json /root/musicat/exe/sha_conf.json && \
     ln -rs /root/musicat/exe/conf/music /root/musicat/exe/music && ./Shasha' $@
