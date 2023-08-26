59994602b4ffc39f5f036b9e1eb1a343701f5c65
Neko-Life <nekolife123579@gmail.com>
2023-08-26 15:54:50 +0700

    init audio processing

---
+++ include/musicat/audio_processing.h
@@ -0,0 +1,12 @@
+#ifndef MUSICAT_AUDIO_PROCESSING_H
+#define MUSICAT_AUDIO_PROCESSING_H
+
+namespace musicat
+{
+namespace audio_processing
+{
+
+} // audio_processing
+} // musicat
+
+#endif // MUSICAT_AUDIO_PROCESSING_H
---
+++ src/musicat/audio_processing.cpp
@@ -0,0 +1,9 @@
+#include "musicat/audio_processing.h"
+
+namespace musicat
+{
+namespace audio_processing
+{
+
+} // audio_processing
+} // musicat
