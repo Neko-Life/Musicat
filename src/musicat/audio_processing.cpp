476da4586a28b909f4e9368114d173f84212f838
Neko-Life <nekolife123579@gmail.com>
2023-09-07 07:56:43 +0700

    set bitrate

--- src/musicat/audio_processing.cpp
+++ src/musicat/audio_processing.cpp
@@ -139,7 +139,7 @@ run_ffmpeg (processor_options_t &options, processor_states_t &p_info)
             (std::string ("volume=")
              + std::to_string ((float)options.volume / (float)100))
                 .c_str (),
-            "-f", "opus", "-ac", "2", "-ar", "48000",
+            "-f", "opus", "-ac", "2", "-ar", "48000", "-b:a", "128k",
             /*"-preset", "ultrafast",*/ "-threads", "1", OUT_CMD,
             (char *)NULL);
 
