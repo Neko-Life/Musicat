919adb7ef4ef5d03d867e8319fb8f1f6d1a9578e
Neko-Life <nekolife123579@gmail.com>
2023-09-10 22:52:45 +0700

    lower buffer to greatly reduce seek latency

--- src/musicat/audio_processing.cpp
+++ src/musicat/audio_processing.cpp
@@ -15,7 +15,7 @@
 #include <thread>
 #include <unistd.h>
 
-#define DPP_AUDIO_BUFFER_LENGTH_SECOND 2.0f
+#define DPP_AUDIO_BUFFER_LENGTH_SECOND 0.3f
 #define SLEEP_ON_BUFFER_THRESHOLD_MS 40
 
 #define BUFFER_SIZE processing_buffer_size
@@ -248,17 +248,49 @@ int
 send_audio_routine (dpp::discord_voice_client *vclient, uint16_t *send_buffer,
                     ssize_t *send_buffer_length, bool no_wait)
 {
+    const bool debug = get_debug_state ();
     bool running_state = get_running_state ();
     // wait for old pending audio buffer to be sent to discord
     if (!no_wait)
-        while ((running_state = get_running_state ()) && vclient
-               && !vclient->terminating
-               && vclient->get_secs_remaining ()
-                      > DPP_AUDIO_BUFFER_LENGTH_SECOND)
-            {
-                std::this_thread::sleep_for (
-                    std::chrono::milliseconds (SLEEP_ON_BUFFER_THRESHOLD_MS));
-            }
+        {
+            float outbuf_duration;
+
+            while ((running_state = get_running_state ()) && vclient
+                   && !vclient->terminating
+                   && ((outbuf_duration = vclient->get_secs_remaining ())
+                       > DPP_AUDIO_BUFFER_LENGTH_SECOND))
+                {
+                    if (debug)
+                        {
+                            static std::chrono::time_point start_time
+                                = std::chrono::high_resolution_clock::now ();
+
+                            auto end_time
+                                = std::chrono::high_resolution_clock::now ();
+
+                            auto done = std::chrono::duration_cast<
+                                std::chrono::milliseconds> (end_time
+                                                            - start_time);
+
+                            start_time = end_time;
+
+                            fprintf (stderr,
+                                     "[audio_processing::send_audio_routine] "
+                                     "outbuf_duration: %f > %f\n",
+                                     outbuf_duration,
+                                     DPP_AUDIO_BUFFER_LENGTH_SECOND);
+
+                            fprintf (
+                                stderr,
+                                "[audio_processing::send_audio_routine] Delay "
+                                "between send: %ld milliseconds\n",
+                                done.count ());
+                        }
+
+                    std::this_thread::sleep_for (std::chrono::milliseconds (
+                        SLEEP_ON_BUFFER_THRESHOLD_MS));
+                }
+        }
 
     if (!running_state || !vclient || vclient->terminating)
         {
