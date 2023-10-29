b7c2804d87ca783982573b0189df06f903f8484f
Neko-Life <nekolife123579@gmail.com>
2023-10-29 19:07:00 +0700

    Revert "remove mutable keyword"
    This reverts commit e01775739c28693afba591e968b7566311089985.

--- src/musicat/server/routes/post_login.cpp
+++ src/musicat/server/routes/post_login.cpp
@@ -168,7 +168,7 @@ post_login (APIResponse *res, APIRequest *req)
     }
 
     res->onData ([res, req, struct_body] (std::string_view chunk,
-                                          bool is_last) {
+                                          bool is_last) mutable {
         std::lock_guard lk (states::recv_body_cache_m);
         std::vector<states::recv_body_t>::iterator cache
             = get_recv_body_cache (struct_body);
