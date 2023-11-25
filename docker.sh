70e6cfa7a93ae0a6bd43565b072502fb506669a4
Neko-Life <nekolife123579@gmail.com>
2023-11-25 07:39:19 +0700

    use postgres alpine

--- docker.sh
+++ docker.sh
@@ -4,11 +4,11 @@
 
 # build the image and pull a postgres image
 docker build . -t shasha/musicat:latest
-docker image pull postgres:14.5-bullseye
+docker image pull postgres:15.2-alpine
 
 # create containers
 docker container create --net net1 -p 9001:9001 -v ./exe/sha_conf.docker.json:/home/musicat/sha_conf.json -v ./exe/music:/home/musicat/music --name Musicat shasha/musicat:latest
-docker container create --net net1 -p 5432:5432 -v /var/lib/postgres/data:/var/lib/postgres/data -e POSTGRES_USER=musicat -e POSTGRES_PASSWORD=musicat -e POSTGRES_DB=musicat --name db postgres:14.5-bullseye
+docker container create --net net1 -p 5432:5432 -v /var/lib/postgres/data:/var/lib/postgres/data -e POSTGRES_USER=musicat -e POSTGRES_PASSWORD=musicat -e POSTGRES_DB=musicat --name db postgres:15.2-alpine
 
 # create network
 docker network create net1
