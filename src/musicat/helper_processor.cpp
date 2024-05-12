faa9171fc38502d6da14e4cf3930908eb8593cab
Neko-Life <nekolife123579@gmail.com>
2024-05-12 23:03:59 +0700

    herr too

--- src/musicat/helper_processor.cpp
+++ src/musicat/helper_processor.cpp
@@ -840,6 +840,8 @@ shutdown_chain (bool discard_output)
                     close_valid_fd (&hci->write_fd);
                     close_valid_fd (&hci->read_fd);
 
+                    kill (hci->pid, SIGTERM);
+
                     int child_status = child::worker::call_waitpid (hci->pid);
 
                     if (debug)
