f660c68af7ede9600024f299d39e944c04a93271
Neko-Life <nekolife123579@gmail.com>
2026-08-07 19:49:33 +0700

    remove unused code

--- src/yt-dlp/utils/ytdlp_run_musicat.py
+++ src/yt-dlp/utils/ytdlp_run_musicat.py
@@ -1,16 +1,6 @@
 from utils.common import printerr
 import utils.ytdlp_run
-
-# !TODO: !!! FIXME: THIS DOESN'T WORK, INCAPABLE OF ASYNC !!!
-import concurrent.futures
-# from threading import Thread
-# from multiprocessing import Process
-
 import musicat
-
-executor = concurrent.futures.ThreadPoolExecutor(thread_name_prefix='mc/ytdlp')
-
-# executor = None
 
 def worker_run(id, url, max_entries, print_stdout, outfile = False):
     try:
@@ -19,27 +9,16 @@ def worker_run(id, url, max_entries, print_stdout, outfile = False):
             musicat.callback(id, res)
         else:
             musicat.callback(id, "")
+        return res
     except Exception:
         musicat.callback(id, "")
+        return None
 
 
 def run(id, url, max_entries, print_stdout, outfile = False):
-    # t = Thread(target=worker_run, name=id, args=[id, url, max_entries, print_stdout, outfile])
-    # t.start()
-
     try:
-        f = executor.submit(worker_run, id, url, max_entries, print_stdout, outfile).result()
+        return worker_run(id, url, max_entries, print_stdout, outfile)
     except Exception:
         musicat.callback(id, "")
-
-    # concurrent.futures.wait([f], 0)
-
-    # global executor
-    # if executor == None:
-    #     executor = Pool(8)
-    # executor.apply_async(worker_run, id, url, max_entries, print_stdout, outfile)
-
-    # Process(target=worker_run, args=(id, url, max_entries, print_stdout, outfile)).start()
-    # Process(target=print, args=(id, url, max_entries, print_stdout, outfile)).start()
-
-    # worker_run(id, url, max_entries, print_stdout, outfile)
+        return None
+    
