4148fe6b88be3bed327af6034611bd6e0c1ca65d
Neko-Life <nekolife123579@gmail.com>
2026-08-11 00:08:50 +0700

    fix better binary protocol

--- src/musicat/server/ws/player.cpp
+++ src/musicat/server/ws/player.cpp
@@ -163,7 +163,7 @@ publish_int32_event (const dpp::snowflake &guild_id, const socket_binary_event_e
 
     const auto s = std::string ({ (char)((n & 0xff000000) >> (3 * 8)), char ((n & 0x00ff0000) >> (2 * 8)),
                                   (char)((n & 0x0000ff00) >> (1 * 8)), (char)((n & 0x000000ff)), '\0' },
-                                4);
+                                5);
 
     // use binary type to reduce bandwidth as this is called around 16 times per second
     publish_event_binary (guild_id, event_binary, s);
